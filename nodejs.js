69dc7fec0783d5e6e6ed32972389e9c4c07453e1
logodragon <49723288+logodragon@users.noreply.github.com>
2020-03-17 05:28:15 +0800

    Create nodejs.js

---
+++ nodejs.js
@@ -0,0 +1,8 @@
+<CENTER>
+<TABLE borderColor=#CCFFCC border=5 borderlight="green">
+  <TBODY>
+  <TR>
+  <TD align=middle><INPUT onClick="showModalDialog('http://www.sohu.com')" type=button value=打开搜狐网站 name=Button>  </TD></TR></TBODY></TABLE>
+</CENTER>
+<CENTER>
+</CENTER>
